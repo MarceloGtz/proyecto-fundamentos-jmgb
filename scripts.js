09d55fa93b275000b028bf0cc0a1a0cb8ac25bef
MarceloGtz <jmezvo@gmail.com>
2022-02-24 17:15:41 -0600

    Corregi el svg de arriba

--- scripts.js
+++
@@ -1,5 +0,0 @@
-// const arrowLeft = document.getElementById("a-left")
-// arrowLeft.addEventListener("click", function(e), console.log(e)) {
-
-// }
-// const arrowRight = document.getElementById("a-right")
